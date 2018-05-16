ae28c7e646c723b9a28409dd0473912fc4c7dcd0
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-05-16 13:41:00 +0200

    Fix collection name for audit events.

--- src/main/java/no/fint/audit/plugin/mongo/MongoAuditEvent.java
+++ src/main/java/no/fint/audit/plugin/mongo/MongoAuditEvent.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
-@Document
+@Document(collection = "auditEvent")
 public class MongoAuditEvent extends AuditEvent {
 
     @Id
