013186df8e459f4a3624292ebfb9680eb46ac48c
Asgeir Nilsen <asgeir@fintlabs.no>
2019-07-04 10:57:53 +0200

    Added some debug logging.

--- src/main/java/no/fint/audit/plugin/mongo/AuditMongoRepository.java
+++ src/main/java/no/fint/audit/plugin/mongo/AuditMongoRepository.java
@@ -1,15 +1,13 @@
 package no.fint.audit.plugin.mongo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-
+@Slf4j
 public class AuditMongoRepository {
 
     @Autowired
@@ -19,7 +17,9 @@ public class AuditMongoRepository {
     private CollectionNameSupplier collectionNameSupplier;
 
     public void save(MongoAuditEvent mongoAuditEvent) {
-        mongoTemplate.save(mongoAuditEvent, collectionNameSupplier.apply(mongoAuditEvent));
+        String collectionName = collectionNameSupplier.apply(mongoAuditEvent);
+        log.debug("Try save to {} - {}", collectionName, mongoAuditEvent);
+        mongoTemplate.save(mongoAuditEvent, collectionName);
     }
 
     @Profile(value = "test")
